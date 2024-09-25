0af3b0ba5ae3ab6ecc9bf926d5f319f2c6811cdf
softskate <lador.org@gmail.com>
2024-09-25 16:06:51 +0500

    Comparing from RAM

--- parse.py
+++ parse.py
@@ -1,8 +1,7 @@
 import json
 import time
 import requests
-import google.generativeai as genai
-from database import Product, Crawl
+from random import choice
 from database import Product, ProductDetails, Similarity
 from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 
@@ -10,20 +9,12 @@ from keys import *
 
 
 def remove_url_parameter(url, param_to_remove):
-    # Parse the URL into its components
     parsed_url = urlparse(url)
-    
-    # Parse the query string into a dictionary
     query_params = parse_qs(parsed_url.query)
-    
-    # Remove the specified parameter
     if param_to_remove in query_params:
         del query_params[param_to_remove]
     
-    # Reconstruct the query string
     new_query_string = urlencode(query_params, doseq=True)
-    
-    # Rebuild the URL without the removed parameter
     new_url = urlunparse(parsed_url._replace(query=new_query_string))
     
     return new_url
@@ -31,29 +22,41 @@ def remove_url_parameter(url, param_to_remove):
 
 class Parser:
     sess = requests.Session()
-    headers = {
-        "User-Agent":"PostmanRuntime/7.40.0",
-        "Accept":"*/*",
-        "Connection":"keep-alive"
-    }
-    sess.headers.update(headers)
-    last_init = 0
+    def __init__(self) -> None:
+        headers = {
+            "User-Agent":"PostmanRuntime/7.40.0",
+            "Accept":"*/*",
+            "Connection":"keep-alive"
+        }
+        self.sess.headers.update(headers)
+        self.update_proxy()
+
+    def update_proxy(self):
+        host, port, login, password = choice(proxies).split(':')
+        self.sess.proxies = {
+            'http': f'http://{login}:{password}@{host}:{port}',
+            'https': f'http://{login}:{password}@{host}:{port}'
+        }
 
     def make_req(self, **kwargs):
-        resp = self.sess.get("https://www.ozon.ru/api/composer-api.bx/page/json/v2", params=kwargs)
-        print('GET->', resp.request.url)
-        print(resp.status_code, resp.content[:100])
-        return resp.json()
+        try:
+            # time.sleep(1)
+            resp = self.sess.get("https://www.ozon.ru/api/composer-api.bx/page/json/v2", params=kwargs, timeout=10)
+            print('GET->', resp.request.url)
+            print(resp.status_code, resp.content[:100])
+            return resp.json()
+        except requests.exceptions.ConnectTimeout:
+            self.update_proxy()
 
 
-    def parse_product_list(self, url, appid, crawlid, page=None):
-        if page is None:
+    def parse_product_list(self, url, appid, crawlid, page=1, retry=10):
+        if page == 1:
             js_data = self.make_req(url=url)
         else:
             js_data = self.make_req(url=url, page=page)
 
         js_data = js_data['widgetStates']
-        page_data = {'items': []}
+        page_data = None
         next_page = {}
         for key in js_data:
             if key.startswith('searchResultsV2'):
@@ -62,6 +65,16 @@ class Parser:
             elif key.startswith('megaPaginator'):
                 next_page = json.loads(js_data[key])
 
+        if retry == 0:
+            return
+        
+        elif not page_data:
+            return self.parse_product_list(url, appid, crawlid, page=page+1, retry=retry-1)
+
+        if not 'items' in page_data:
+            print('No items found')
+            return
+        
         for prod in page_data['items']:
             link = 'https://ozon.ru' + prod['action']['link']
             sku = prod['skuId']
@@ -82,10 +95,11 @@ class Parser:
                                 price_data = price_data.replace(x, '')
                             price = int(price_data)
 
-            for img in prod['tileImage']['items']:
-                if img['type'] == 'image':
-                    image = img['image']['link']
-                    break
+            if 'items' in prod['tileImage']:
+                for img in prod['tileImage']['items']:
+                    if img['type'] == 'image':
+                        image = img['image']['link']
+                        break
             
             uprod = Similarity.get_or_none((Similarity.productId == sku) & ((Similarity.uprice*1.01) >= price))
             if uprod:
@@ -112,8 +126,6 @@ class Parser:
                 except Exception as e:
                     print(f"Failed to send message: {e}")
 
-            
-            Product.get_or_none(crawlid)
             item = {}
             item["appid"] = appid
             item["crawlid"] = crawlid
@@ -127,9 +139,8 @@ class Parser:
             if not exist:
                 self.parse_product_details(link, appid, crawlid)
 
-        if next_page:
-            next_page = parse_qs(next_page['nextPage'])['page'][0]
-            self.parse_product_list(url, appid, crawlid, next_page)
+        if page_data:
+            return self.parse_product_list(url, appid, crawlid, page=page+1)
             
 
     def parse_product_details(self, url, appid, crawlid):
@@ -184,10 +195,23 @@ def get_unit_products():
     return out
 
 
-genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel()
 class AISimilar:
     def __init__(self):
+        self.api_index = 0
+        self.update()
+
+
+    def update(self):
+        self.session = requests.Session()
+        host, port, login, password = proxies[self.api_index%len(proxies)].split(':')
+        self.session.proxies = {
+            'http': f'http://{login}:{password}@{host}:{port}',
+            'https': f'http://{login}:{password}@{host}:{port}'
+        }
+        print(self.session.proxies)
+
+
+    def check_products(self, likes, uprod) -> list:
         msg = ('Есть список электроники в таком виде:\n'
                 '21: iPhone 14 pro max 256GB\n'
                 '52: iPhone 14 promax 256GB\n'
@@ -198,18 +222,63 @@ class AISimilar:
                 'Из списка надо найти те которые '
                 'один и тот же вешь с образцом и в ответе '
                 'отправить json со списком нумерации '
-                'подходящих товаров если они есть как это:\n'
-                '[21, 52, 85]')
+                'подходящих товаров как это:\n'
+                '[21, 52, 85]'
+            )
 
-        self.chat = model.start_chat(history=[
-                {"role": "model", "parts": [msg]}
-            ])
+        payload = {
+            "contents": [
+                {"role": "model", "parts": [{"text": msg}]},
+                {"role": "user", "parts": [{"text": f"{x.productId}: {x.name}"} for x in likes]},
+                {"role": "user", "parts": [{"text": f"Образец: {uprod['name']}"}]}
+            ],
+            "safetySettings": [
+                {
+                    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                    "threshold": "BLOCK_NONE"
+                },
+                {
+                    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                    "threshold": "BLOCK_NONE"
+                },
+                {
+                    "category": "HARM_CATEGORY_HATE_SPEECH",
+                    "threshold": "BLOCK_NONE"
+                },
+                {
+                    "category": "HARM_CATEGORY_HARASSMENT",
+                    "threshold": "BLOCK_NONE"
+                },
+            ],
+            "generationConfig": {
+                "temperature": 0.8,
+                "maxOutputTokens": 800,
+                "topP": 0.8,
+                "topK": 10
+            }
+        }
+        print('Making request using', GOOGLE_API_KEY[self.api_index%len(GOOGLE_API_KEY)])
+        resp = self.session.post("https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + GOOGLE_API_KEY[self.api_index%len(GOOGLE_API_KEY)], 
+                                 json=payload)
+        open('ai.json', 'wb').write(resp.content)
+        js_data = json.loads(resp.content)
+        output = []
+        if 'error' in js_data:
+            if js_data['error']['code'] == 429:
+                print("Too many requests, waiting 10 seconds...")
+                time.sleep(10)
+                self.api_index += 1
+                self.update()
+                # return self.check_products(likes, uprod)
+            return []
 
+        for x in js_data['candidates']:
+            for text in x['content']['parts']:
+                try: output.extend(json.loads(text['text']))
+                except json.decoder.JSONDecodeError: return []
 
-    def check_products(self, likes, uprod) -> list:
-        likes = [f"{x.productId}: {x.name}" for x in likes]
-        likes.append(f"Образец: {uprod['name']}")
-        response = self.chat.send_message({"role": "user", "parts": likes})
-        return json.loads(response)
-
-
+        if output:
+            print(payload)
+            print('\n'*5)
+            print(js_data['candidates'])
+        return list(set(output))
--- spider.py
+++ spider.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta
-import json
 import time
 from difflib import SequenceMatcher
-from peewee import fn, JOIN
+import requests
 from database import ParsingItem, App, Crawl, Product, Similarity, db
 from parse import Parser, get_unit_products, AISimilar
 
@@ -12,7 +11,15 @@ ai = AISimilar()
 
 
 def similarity(a, b):
-    return SequenceMatcher(None, a, b).ratio()
+    a, b = a.lower().split(), b.lower().split()
+    a, b = (a, b) if len(a) > len(b) else (b, a)
+    answer = 0
+    for i in b:
+        for x in a:
+            if SequenceMatcher(None, i, x).ratio() > .5:
+                answer += 1
+    answer = answer/len(b)
+    return answer
 
 
 def run_spider():
@@ -23,60 +30,92 @@ def run_spider():
             .delete()
             .where(Product.crawlid.in_(old_crawlers)))
         dq.execute()
+
+
         urls = [x.link for x in ParsingItem.select()]
-        crawl = Crawl.create()
-        crawlid = crawl.get_id()
-        while urls:
-            scrape_url = urls.pop()
-            app = App.create(
-                name = 'Ozon',
-                start_url = scrape_url
-            )
-            appid = app.get_id()
-            while True:
-                try:
-                    print(f'Scraping url: {scrape_url}')
-                    parser.parse_product_list(scrape_url, appid, crawlid)
-                    break
-                except Exception as e:
-                    print(f'Error occurred while scraping: {e}')
-                    time.sleep(5)
+        crawl = (
+            Crawl
+            .select()
+            .where(Crawl.finished == True)
+            .order_by(Crawl.created_at.desc())
+            .first()
+        )
+        print(crawl.created_at)
+        if crawl and crawl.created_at < datetime.now() - timedelta(hours=10):
+            crawl = Crawl.create()
+            crawlid = crawl.get_id()
+            while urls:
+                scrape_url = urls.pop()
+                app = App.create(
+                    name = 'Ozon',
+                    start_url = scrape_url
+                )
+                appid = app.get_id()
+                while True:
+                    try:
+                        print(f'Scraping url: {scrape_url}')
+                        parser.parse_product_list(scrape_url, appid, crawlid)
+                        break
+                    except Exception as e:
+                        print(f'Error occurred while scraping: {e}')
+                        time.sleep(5)
 
-            time.sleep(60)
+                if urls: time.sleep(60)
 
-        db.connect(True)
-        crawl.finished = True
-        crawl.save()
+            db.connect(True)
+            crawl.finished = True
+            crawl.save()
 
+        query = list(Product
+            .select()
+            .where(Product.crawlid == crawl)
+        )
+
+        similars = {}
+        for sim in Similarity.select():
+            if sim.productId in similars:
+                similars[sim.productId].append(sim.unitArticle)
+            else:
+                similars[sim.productId] = [sim.unitArticle]
+
+        print('Checking for similarity')
         for uprod in get_unit_products():
             for old_prod in Similarity.select().where(Similarity.unitArticle == uprod['article']):
                 if old_prod.uprice != uprod['price']:
                     old_prod.uprice = uprod['price']
 
-            query = (Product
-                .select()
-                .join(Similarity, JOIN.LEFT_OUTER, on=(Similarity.productId == Product.productId))
-                .where(
-                    (fn.similarity(Product.name, uprod['name']) >= 0.7)
-                     & (Product.crawlid == crawl)
-                     & (Similarity.unitArticle != uprod['article'])
-                )
-            )
-            if query:
-                similar = ai.check_products(query)
-                for productId in similar:
-                    Similarity.create(
-                        productId = productId,
-                        unitArticle = uprod['article'],
-                        uprice = uprod['price']
-                    )
-        
+            likes = []
+            for prod in query:
+                old_sims = similars.get(prod.productId, [])
+                if uprod['article'] in old_sims: continue
+                if similarity(prod.name, uprod['name']) >= 0.7:
+                    likes.append(prod)
+            
+            if likes:
+                similar = ai.check_products(likes, uprod)
+                if similar:
+                    payload = {uprod['article']: []}
+                    for productId in similar:
+                        Similarity.create(
+                            productId = productId,
+                            unitArticle = uprod['article'],
+                            uprice = uprod['price']
+                        )
+                        prod_data = Product.get(productId=productId)
+                        payload[uprod['article']].append(prod_data.productUrl)
+                    try:
+                        stas_response = requests.post('http://92.53.64.89:5003/recieve_ozon_competitors', json=payload, timeout=3)
+                        print(stas_response.status_code, stas_response.content[:50])
+                    except Exception as e: print('Reciving system error')
+        print('Finished checking')
         time.sleep(60*60)
 
 
 if __name__ == '__main__':
     db.register_function(similarity, 'similarity', 2)
     while True:
-        try: run_spider()
-        except Exception as e: print(f'Unexpected exception occurred {e}')
+        try: 
+            run_spider()
+        except Exception as e:
+            print(f'Unexpected exception occurred {e}')
         time.sleep(5)
